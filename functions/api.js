34bf41a23eba57de7e2ae1631dc18b277f6a4221
DMT7121 <dmt7121@gmail.com>
2026-06-08 17:43:25 +0700

    Update Google Apps Script URL to deployment ID @13

--- functions/api.js
+++ functions/api.js
@@ -29,7 +29,7 @@ export async function onRequest(context) {
     const bodyText = await request.text();
     
     // Target Google Apps Script Web App URL (Authorized Public Deployment)
-    let gasUrl = "https://script.google.com/macros/s/AKfycbztN-khJ8QEvj08c02GVvvLvAOKZZ7O9ZMMeUFAMMmtCWR7V5LWSWQ60eOkOMcedx-4CQ/exec";
+    let gasUrl = "https://script.google.com/macros/s/AKfycbxs3owOv97T2AO6e4_fEj2b5A3dDYwmXOUsWVcK47BiW_W1XLiGmarJr5Z8v-VXkPXy5Q/exec";
     
     // Fetch with manual redirect mode to preserve POST method across 302 redirects
     let response = await fetch(gasUrl, {
